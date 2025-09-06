912e1fa9a75ba8e063edbd548b1aaccc4ea3d6c0
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-09-06 13:37:12 -0700

    Create new component About

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+const About = () => {
+    return (
+        <section id="about" className="py-24 bg-gray-50">
+            <div className="container mx-auto px-4">
+                <div className="text-center mb-16">
+                    <h2 className="section-title">About Our Business</h2>
+                    <p className="section-subtitle">
+                        Learn more about our experience and commitment to quality
+                    </p>
+                </div>
+                <div className="text-center">
+                    <p>About component will be implemented here</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
