4b2db17936059f9f3889eec47192beb0a2293e20
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-09-06 13:41:16 -0700

    Create new component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white py-12">
+            <div className="container mx-auto px-4">
+                <div className="text-center">
+                    <p>&copy; 2025 Uncle's Decorations. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
