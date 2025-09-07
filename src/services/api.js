20f35fb89c08fc27f22cbec91174b22283df46d2
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-09-06 21:39:12 -0700

    Update variable of API_BASE to API_BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
