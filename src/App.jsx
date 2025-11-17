33ea78115fc7b002d2e14083539ab100033abb62
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-11-16 19:22:13 -0800

    Split files: AuthContext and useAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import { ProtectedRoute } from "@/components/admin/ProtectedRoute";
 import AdminLayout from "@/components/admin/AdminLayout";
